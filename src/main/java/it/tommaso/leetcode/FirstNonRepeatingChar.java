58cf81617ce001c364ef83f6c44ef8dfebf4192e
Tommaso Bonato <tommasobonato95@gmail.com>
2018-07-16 00:11:14 +0200

    First easy problem leetcode

--- src/main/java/it/tommaso/leetcode/FirstNonRepeatingChar.java
+++ src/main/java/it/tommaso/leetcode/FirstNonRepeatingChar.java
@@ -1,5 +1,12 @@
 package it.tommaso.leetcode;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * Created by Tommaso Bonato on 15/07/2018.
  *
@@ -18,9 +25,9 @@ class FirstNonRepeatingChar {
      * @param string Word to analyze
      * @return index of the first non repeating char. -1 if not present.
      */
-    int simpleSolution(String string) {
+    int slowSolution(String string) {
 
-        boolean foundAnother = false;
+        boolean foundAnother;
         char[] charArray = string.toCharArray();
 
         for (int i = 0; i < string.length(); i++) {
@@ -37,6 +44,79 @@ class FirstNonRepeatingChar {
         }
 
         return -1;
+    }
 
+    /**
+     * Simple implementation using a support array. Works only with the 26 letters from the english alphabet or
+     * a set of chars of max 26 different chars.
+     * Space complexity: O(1) -> Only because we are using a fixed array
+     * Time complexity: O(n)
+     *
+     * @param string Word to analyze
+     * @return index of the first non repeating char. -1 if not present.
+     */
+    int solutionWithSupportArray(String string) {
+
+        char[] countChar = new char[26];
+        string = string.toLowerCase();
+        for (int i = 0; i < string.length(); i++) {
+            countChar[string.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < string.length(); i++) {
+            if (countChar[string.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
+    /**
+     * Slightly more advanced implementation using a hashmap
+     * Space complexity: O(n)
+     * Time complexity: O(n)
+     *
+     * @param string Word to analyze
+     * @return index of the first non repeating char. -1 if not present.
+     */
+    int hashMapSolution(String string) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < string.length(); i++) {
+            map.put(string.charAt(i), map.getOrDefault(string.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < string.length(); i++) {
+            if (map.get(string.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Advanced implementation using a LinkedHashMap and Java8
+     * Space complexity: O(n)
+     * Time complexity: O(n)
+     *
+     * @param string Word to analyze
+     * @return index of the first non repeating char. -1 if not present.
+     */
+    int linkedHashMapSolution(String string) {
+
+        Map<Character, Long> collect =  string.chars().mapToObj(i -> (char)i).
+                collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
+
+        Optional<Character> firstUnique = collect.entrySet().stream().
+                filter(e -> e.getValue() == 1).map(Map.Entry::getKey).findFirst();
+
+        for (int i = 0; i < string.length(); i++) {
+            if (firstUnique.isPresent() && (string.charAt(i) == firstUnique.get())) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- src/test/java/it/tommaso/leetcode/Test.java
+++ src/test/java/it/tommaso/leetcode/Test.java
@@ -15,16 +15,33 @@ public class Test {
         FirstNonRepeatingChar classInstance = new FirstNonRepeatingChar();
 
         /*
-         * Testing 3 scenarios:
+         * Testing 4 scenarios:
          * - Empty String
          * - Normal String with repeating chars
          * - String with only unique chars
          * - String with only repeating chars
+         * - Long random stream with unique chars
          */
-        assertEquals(-1, classInstance.simpleSolution(""));
-        assertEquals(6, classInstance.simpleSolution("mammamia"));
-        assertEquals(0, classInstance.simpleSolution("ciao"));
-        assertEquals(-1, classInstance.simpleSolution("aabbccddeeffghhiillmmg"));
+        assertEquals(-1, classInstance.slowSolution(""));
+        assertEquals(6, classInstance.slowSolution("mammamia"));
+        assertEquals(0, classInstance.slowSolution("ciao"));
+        assertEquals(-1, classInstance.slowSolution("aabbccddeeffghhiillmmg"));
+
+        assertEquals(-1, classInstance.solutionWithSupportArray(""));
+        assertEquals(6, classInstance.solutionWithSupportArray("mammamia"));
+        assertEquals(0, classInstance.solutionWithSupportArray("ciao"));
+        assertEquals(-1, classInstance.solutionWithSupportArray("aabbccddeeffghhiillmmg"));
+
+        assertEquals(-1, classInstance.hashMapSolution(""));
+        assertEquals(6, classInstance.hashMapSolution("mammamia"));
+        assertEquals(0, classInstance.hashMapSolution("ciao"));
+        assertEquals(-1, classInstance.hashMapSolution("aabbccddeeffghhiillmmg"));
+
+        assertEquals(-1, classInstance.linkedHashMapSolution(""));
+        assertEquals(6, classInstance.linkedHashMapSolution("mammamia"));
+        assertEquals(0, classInstance.linkedHashMapSolution("ciao"));
+        assertEquals(-1, classInstance.linkedHashMapSolution("aabbccddeeffghhiillmmg"));
+
     }
 
 }
