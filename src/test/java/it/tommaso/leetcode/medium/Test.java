870e430591e4443af1455b8e0834b6d4e5fdc382
Tommaso Bonato <tommasobonato95@gmail.com>
2018-07-19 23:57:57 +0200

    Fixed one problem with a test.

--- src/test/java/it/tommaso/leetcode/medium/Test.java
+++ src/test/java/it/tommaso/leetcode/medium/Test.java
@@ -13,11 +13,14 @@ public class Test {
         AllDuplicatesArray allDuplicatesArray = new AllDuplicatesArray();
 
         int[] answer = allDuplicatesArray.getDuplicates(new int[]{1,2,4,5,2,1,6}).stream().mapToInt(i -> i).toArray();
+        Arrays.sort(answer);
         assertTrue(Arrays.equals(new int[]{1,2}, answer));
-        answer = allDuplicatesArray.getDuplicates(new int[]{1,2,4,5}).stream().mapToInt(i -> i).toArray();
+        answer = allDuplicatesArray.getDuplicates(new int[]{1,2,4,3}).stream().mapToInt(i -> i).toArray();
+        Arrays.sort(answer);
         assertTrue(Arrays.equals(new int[]{}, answer));
         answer = allDuplicatesArray.getDuplicates(new int[]{1,1,1,1,2,1,2}).stream().mapToInt(i -> i).toArray();
-        assertTrue(Arrays.equals(new int[]{1,2}, answer));
+        Arrays.sort(answer);
+        assertTrue(Arrays.equals(new int[]{1,1,1,1,2}, answer));
 
     }
 }
